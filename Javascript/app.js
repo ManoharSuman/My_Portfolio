0e60bfdf2ba5192c04040c4b44ca305869a5b9a2
Manohar Suman <happymanohar.ms@gmail.com>
2020-07-30 23:36:18 +0530

    Mob-Nav

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,11 +1,20 @@
+$(document).ready(function(){
+$('.slider').slick({
+    arrows:false,
+    dots:true,
+    appendDots:'.slider-dots',
+    dotsClass:'dots'
+});
 let hamberger = document.querySelector('.hamberger');
 let times = document.querySelector('.times');
-let mabileNav = document.querySelector('.mobile-nav')
+let mobileNav = document.querySelector('.mobile-nav')
 
 hamberger.addEventListener('click', function(){
-    mobileNav.classList.add('.open');
+    mobileNav.classList.add('open');
 
 });
 times.addEventListener('click', function(){
-    mobileNav.classList.remove('.open');
+    mobileNav.classList.remove('open');
+});
+
 });

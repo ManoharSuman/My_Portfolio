5c5dd6be7f667a14892b23a16db573b3e2f873f7
Manohar Suman <happymanohar.ms@gmail.com>
2020-07-30 15:27:34 +0530

    MobNavigation-2

---
+++ Javascript/app.js
@@ -0,0 +1,11 @@
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mabileNav = document.querySelector('.mobile-nav')
+
+hamberger.addEventListener('click', function(){
+    mobileNav.classList.add('.open');
+
+});
+times.addEventListener('click', function(){
+
+});
